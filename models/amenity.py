5f842444b64b34b1c0382e07b9b8636cd6142bba
HajarElMannani <laso125@hotmail.com>
2025-01-05 10:35:08 -0800

    add attributes

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 '''class Amenity'''
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''class that inherits from BaseModel
     Attributes:
         name(str): empty string'''
 
-    name = ""
+    __tablename__ == "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
