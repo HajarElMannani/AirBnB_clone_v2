8f4c244b93689a0ec840ef88696bba52226ff31c
HajarElMannani <laso125@hotmail.com>
2025-01-23 12:27:53 -0800

    fix error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-'''Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy'''
+'''Fabric script (based on the file 1-pack_web_static.py) that
+ distributes an archive to your web servers, using the function
+ do_deploy'''
 from fabric.api import put, run
 from fabric.api import env
 from os import path
+
 env.hosts = ['54.236.45.210', '54.197.82.208']
 
 
@@ -13,18 +16,19 @@ def do_deploy(archive_path):
         return False
     try:
         file_path = archive_path.split('/')[-1]
-        put(archive_path, f'/tmp/{file_path}')
         file_extract = file_path.split('.')[0]
-        extracted_file = "data/web_static/releases/{}/".format(file_extract)
-        run(f'rm -rf {extracted_file}/')
-        run(f'mkdir -p {extracted_file}/')
-        run(f'tar -xzf /tmp/{file_path} -C {extracted_file}')
-        run(f'rm  /tmp/{file_path}')
-        run(f'mv {extracted_file}web_static/* {extracted_file}')
-        run(f'rm -rf {extracted_file}web_static')
-        run('rm -rf /data/web_static/current')
-        run(f'ln -s {extracted_file} /data/web_static/current')
-    except Exception:
+        extracted_file = "/data/web_static/releases/{}/".format(file_extract)
+        put(archive_path, f"/tmp/{file_path}")
+        run(f"rm -rf {extracted_file}/")
+        run(f"mkdir -p {extracted_file}")
+        run(f"tar -xzf /tmp/{file_path} -C {extracted_file}")
+        run(f"rm  /tmp/{file_path}")
+        run(f"mv {extracted_file}web_static/* {extracted_file}")
+        run(f"rm -rf {extracted_file}web_static")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {extracted_file} /data/web_static/current")
+    except Exception as e:
+        print("Error: {}".format(e))
         return False
+    print("New version deployed!")
     return True
-    
