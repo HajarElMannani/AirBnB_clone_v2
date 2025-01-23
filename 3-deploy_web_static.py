182e7dad002a579f7456c68f1fc761ea7e80ceb9
HajarElMannani <laso125@hotmail.com>
2025-01-23 13:51:01 -0800

    First comit

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''Fabric script that creates and distributes an archive to your web servers, using the function deploy'''
+from fabric.api import local
+from datetime import datetime
+from fabric.api import put, run
+from fabric.api import env
+from os import path
+
+env.hosts = ['54.236.45.210', '54.197.82.208']
+
+
+def do_pack():
+    '''generates a .tgz archive from the contents
+    of the web_static folder'''
+    time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = f"versions/web_static_{time_now}.tgz"
+    local('mkdir -p versions')
+    try:
+        local(f'tar -cvzf {archive_name} web_static')
+    except Exception:
+        return None
+    return archive_name
+
+def do_deploy(archive_path):
+    '''distributes an archive to your web servers,
+    using the function do_deploy'''
+    if (path.isfile(archive_path) is False):
+        return False
+    try:
+        file_path = archive_path.split('/', 1)[-1]
+        file_extract = file_path.split('.', 1)[0]
+        extracted_file = "/data/web_static/releases/{}/".format(file_extract)
+        put(archive_path, "/tmp/{}".format(file_path))
+        run("rm -rf {}".format(extracted_file))
+        run("mkdir -p {}".format(extracted_file))
+        run("tar -xzf /tmp/{} -C {}".format(file_path, extracted_file))
+        run("rm  /tmp/{}".format(file_path))
+        run("mv {}web_static/* {}".format(extracted_file, extracted_file))
+        run("rm -rf {}web_static".format(extracted_file))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(extracted_file))
+        print("New version deployed!")
+    except Exception as e:
+        print("Error: {}".format(e))
+        return False
+    return True
+
+def deploy():
+    '''reates and distributes an archive to your web servers'''
+    arch_path = do_pack()
+    if arch_path is None:
+        return False
+    return do_deploy(arch_path)
