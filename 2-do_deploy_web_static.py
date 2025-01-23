fb1bb5f5b73677babff3238c630fb74a6a4a3c6d
HajarElMannani <laso125@hotmail.com>
2025-01-23 12:51:12 -0800

    change format

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,20 +15,20 @@ def do_deploy(archive_path):
     if (path.isfile(archive_path) is False):
         return False
     try:
-        file_path = archive_path.split('/')[-1]
-        file_extract = file_path.split('.')[0]
+        file_path = archive_path.split('/', 1)[-1]
+        file_extract = file_path.split('.', 1)[0]
         extracted_file = "/data/web_static/releases/{}/".format(file_extract)
-        put(archive_path, f"/tmp/{file_path}")
-        run(f"rm -rf {extracted_file}/")
-        run(f"mkdir -p {extracted_file}")
-        run(f"tar -xzf /tmp/{file_path} -C {extracted_file}")
-        run(f"rm  /tmp/{file_path}")
-        run(f"mv {extracted_file}web_static/* {extracted_file}")
-        run(f"rm -rf {extracted_file}web_static")
+        put(archive_path, "/tmp/{}".format(file_path))
+        run("rm -rf {}".format(extracted_file))
+        run("mkdir -p {}".format(extracted_file))
+        run("tar -xzf /tmp/{} -C {}".format(file_path, extracted_file))
+        run("rm  /tmp/{}".format(file_path))
+        run("mv {}web_static/* {}".format(extracted_file, extracted_file))
+        run("rm -rf {}web_static".format(extracted_file))
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {extracted_file} /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(extracted_file))
+        print("New version deployed!")
     except Exception as e:
         print("Error: {}".format(e))
         return False
-    print("New version deployed!")
     return True
