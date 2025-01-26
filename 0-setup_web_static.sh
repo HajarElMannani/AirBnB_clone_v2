5dff8de613aa43da09c4f2d41796bf793256cea5
HajarElMannani <laso125@hotmail.com>
2025-01-26 13:57:02 -0800

    Rectification

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Script that sets up your web servers for the deployment of web_static.
+#Script that sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/
@@ -8,8 +8,7 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-cat << EOF | sudo tee /data/web_static/releases/test/index.html
-<!DOCTYPE html>
+cat << 'EOF' | sudo tee /data/web_static/releases/test/index.html
 <html>
   <head>
   </head>
@@ -18,30 +17,8 @@ ALX
   </body>
 </html>
 EOF
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R 'ubuntu':'ubuntu' /data/
-cat <<EOF | sudo tee /etc/nginx/sites-available/default
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm;
-    add_header X-Served-By $(hostname);
-    location = /data/web_static/current/ {
-    	     alias /hbnb_static/;
-	     index index.html index.htm;
-    }
-    location = /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-        default_type text/html;
-        return 404 "Ceci n'est pas une page";
-    }
-}
-EOF
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
