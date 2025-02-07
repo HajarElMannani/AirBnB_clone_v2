ca62cf1bd35a1f07d167a234003d661368c0022b
HajarElMannani <laso125@hotmail.com>
2025-02-07 12:52:22 -0800

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,17 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-<<<<<<< HEAD
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-=======
-    '''Function that displays Hello HBNB'''
-    return "Hello HBNB!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
->>>>>>> c679e13c9cf25ea1cb9827b9510fd4f58eec8637
