ee9825f1689227926a9289a474b9d42742339684
HajarElMannani <laso125@hotmail.com>
2025-02-07 21:34:23 +0000

    First commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,10 +5,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    '''Function that displays Hello HBNB'''
     return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
