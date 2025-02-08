73b24dd968b4e1a5ee9f59406266754966be225d
HajarElMannani <laso125@hotmail.com>
2025-02-08 06:38:35 -0800

    Change code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,16 +19,18 @@ def do_deploy(archive_path):
     try:
         file_path = archive_path.split('/', 1)[-1]
         file_extract = file_path.split('.', 1)[0]
-        extracted_file = "/data/web_static/releases/{}/".format(file_extract)
-        put(archive_path, "/tmp/{}".format(file_path))
-        run("rm -rf {}".format(extracted_file))
-        run("mkdir -p {}".format(extracted_file))
-        run("tar -xzf /tmp/{} -C {}".format(file_path, extracted_file))
+        extracted_file = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}".format(extracted_file, file_extract))
+        run("tar -xzf /tmp/{} -C {}{}".format(file_path, extracted_file,
+                                              file_extract))
         run("rm  /tmp/{}".format(file_path))
-        run("mv {}web_static/* {}".format(extracted_file, extracted_file))
-        run("rm -rf {}web_static".format(extracted_file))
+        run("mv {0}{1}/web_static/* {0}{1}".format(extracted_file,
+                                                   file_extract))
+        run("rm -rf {}{}/web_static".format(extracted_file, file_extract))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(extracted_file))
+        run("ln -s {}{}/ /data/web_static/current".format(extracted_file,
+                                                          file_extract))
         print("New version deployed!")
     except Exception as e:
         print("Error: {}".format(e))
